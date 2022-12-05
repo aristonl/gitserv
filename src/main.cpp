a5f99f51deb08747c9b066a97f290c88d7a4ccd3
Ariston Lorenzo <aristonl@n11.dev>
2022-12-04 21:59:44 -0500

    Meta: build the webpage
    Signed-off-by: Ariston Lorenzo <aristonl@n11.dev>

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <link>
+#include <iostream>
+
+int main() {
+	Link http(3000);
+
+  	// 404 Page
+  	http.Error(404, [](Request* req, Response* res) {
+    	res->SetHTTP("HTTP/1.1 404 Not Found\r\n\r\n404 Not Found");
+  	});
+  	http.Default([](Request* req, Response* res) {
+    	res->Error(404);
+  	});
+  
+  	// Landing Page
+  	http.Get("/", [](Request* req, Response* res) {
+    	res->SetHeader("Content-Type", "text/html; charset=UTF-8");
+    	res->SendFile("www/index.html");
+  	});
+
+  	http.Get("/css/style.css", [](Request* req, Response* res) {
+   	 res->SetHeader("Content-Type", "text/css; charset=UTF-8");
+  	  res->SendFile("www/css/index.css");
+  	});
+
+	std::cout << "Starting server!" << std::endl;
+  	http.Start();
+}
