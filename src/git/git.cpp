46b35444a64e5e30ddd4213b146cebd96632bfaf
Ariston Lorenzo <4tl0renz0@gmail.com>
2022-12-31 22:18:46 -0500

    git: Check if a folder is a bare repository
    I gotta figure out how I can go thru every repository in a user folder
    and check if they are a proper bare repository. Right now it's only
    taking direct paths (i.e. /srv/git/n11/link.git) but not going thru
    every repository folder or user folder.
    
    Signed-off-by: Ariston Lorenzo <4tl0renz0@gmail.com>

--- src/git/git.cpp
+++ src/git/git.cpp
@@ -1,5 +1,7 @@
 #include <git/git.h>
 
+#include <git2.h>
+
 #include <dirent.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,7 +10,22 @@
 #include <string>
 #include <cstring>
 
+/*
+ * The way the /srv/git directory is structured is as follows:
+ * /srv/git
+ * |--/user1
+ * |  |--/repo1.git
+ * |  |--/repo2.git
+ * |--/user2
+ * |  |--/repo1.git
+ * |  |--/repo2.git
+ * |--/org
+ *    |--/repo1.git
+ */
+
 int initRepos() {
+    git_libgit2_init();
+
 	//const char* gitRepoLoc = "/srv/git";
 	const std::string gitRepoLoc = "/srv/git";
 
@@ -26,43 +43,66 @@ int initRepos() {
 		}
 	}
 
-	DIR* dir = opendir(gitRepoLoc.c_str());
- 	if (dir == nullptr) {
-    	printf("Error: Failed to open /srv/git.\n");
-    	return -1;
-  	}
+    // Enter /srv/git
+    DIR* dir = opendir(gitRepoLoc.c_str());
+    if (dir == NULL) {
+        printf("Cannot open /srv/git. Make sure you are running with sudo or as root.\n");
+        return -1;
+    }
 
-	// Iterate over the folders in the root directory
-  	struct dirent* entry;
-  	while ((entry = readdir(dir)) != nullptr) {
-    	// Skip . and .. entries
-    	if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-      		continue;
-    	}
+    // Go thru the user folders in /srv/git
+    struct dirent* ent;
+    while ((ent = readdir(dir)) != NULL) {
+        if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) {
+            continue;
+        }
 
-    	// Check if the current entry is a folder
-    	if (entry->d_type == DT_DIR) {
-      		// Change to the folder
-      		std::string folderPath = gitRepoLoc + "/" + entry->d_name;
-      		if (chdir(folderPath.c_str()) != 0) {
-        		//std::cerr << "Error: Failed to change to directory '" << folder_path << "'" << std::endl;
-				printf("Error: Falied to change to directory '%s'.", folderPath);
-        		continue;
-      		}
+        if (ent->d_type == DT_DIR) {
+            std::string userFolder = gitRepoLoc + "/" + ent->d_name;
+            printf("Found user folder: %s\n", userFolder.c_str());
+            if (chdir(userFolder.c_str()) != 0) {
+                printf("Cannot change to user folder: %s. Make sure you are running with sudo or as root.\n", userFolder.c_str());
+                return -1;
+            }
 
-      		// Check if the folder is a bare repository
-      		int result = system("git rev-parse --is-bare-repository");
-      		if (result == 0) {
-        		//std::cout << "The folder '" << folder_path << "' is a bare repository" << std::endl;
-				printf("Discovered repository '%s'", folderPath);
-      		} else {
-     			//std::cout << "The folder '" << folder_path << "' is not a bare repository" << std::endl;
-				printf("Folder '%s' isn't a Git repository. Skipping...", folderPath);
-      		}
-   	 	}
-	}
+            DIR* userDir = opendir(userFolder.c_str());
+            if (userDir == NULL) {
+                printf("Cannot open user folder: %s. Make sure you are running with sudo or as root.\n", userFolder.c_str());
+                return -1;
+            }
 
-  	closedir(dir);
+            struct dirent* userEnt;
+            while ((userEnt = readdir(userDir)) != NULL) {
+                if (strcmp(userEnt->d_name, ".") == 0 || strcmp(userEnt->d_name, "..") == 0) {
+                    continue;
+                }
+
+                // FIXME: This currently doesn't iterate thru *every* directory/repository in a user's folder.
+                if (userEnt->d_type == DT_DIR) {
+                    git_repository* repository;
+                    std::string repoFolder = userFolder + "/" + userEnt->d_name;
+                    printf("Found repo folder: %s\n", repoFolder.c_str());
+                    int error = git_repository_open(&repository, repoFolder.c_str());
+                    if (error < 0) {
+                        printf("Failed to open repository: %s\n", repoFolder.c_str());
+                        return 1;
+                    }
+
+                    // Check if the repository is a bare repository
+                    bool is_bare = git_repository_is_bare(repository);
+                    if (is_bare) {
+                        printf("Repository is bare.\n");
+                    } else {
+                        printf("Repository is not bare.\n");
+                    }
+
+                    return 0;
+                }
+            }
+        } else {
+            printf("Cannot find any user directories.\n");
+        }
+    }
 
 	return 0;
 }
