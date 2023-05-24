f07f08f7cfbd9936277376ff238c4b2c83779ba7
Ariston Lorenzo <me@ariston.dev>
2023-05-24 00:29:09 -0400

    src: Update for Link 2.0
    Signed-off-by: Ariston Lorenzo <me@ariston.dev>

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <gitserv.h>
 #include <iostream>
-#include <link>
+#include <fstream>
+#include <Link.hpp>
 
 #include <cstring>
 #include <unistd.h>
@@ -10,87 +11,51 @@ int main(int argc, char* argv[])
     // FIXME: init() is causing the web server to give 404's
 	//init();
 
-    //int opt;
-    //char* publicKeyPath = NULL;
-
-    // FIXME: the code commented out below causes a seg fault
-    /*
-    if (strcmp(argv[1], "setup") == 0) {
-        while ((opt = getopt(argc, argv, "k:")) != -1) {
-            switch (opt) {
-                case 'k':
-                    publicKeyPath = optarg;
-                    break;
-            }
-        }
-        printf("Public key path: %s\n", publicKeyPath);
-        setup(publicKeyPath);
-    }
-     */
-
-	Link http(3000);
+	Link::Server server;
+	server.SetPort(3000);
 
 	// 404 Page
-	http.Error(404, [](Request* req, Response* res) {
-		res->SetHTTP("HTTP/1.1 404 Not Found\r\n\r\n404 Not Found");
-        printf("404 Not Found");
+	server.Error(404, [](Link::Request* req, Link::Response* res) {
+		res->SetStatus(404)->SetBody("404 Not Found");
 	});
-	http.Default([](Request* req, Response* res) { res->Error(404); });
 
 	// Landing Page
-	http.Get("/", [](Request* req, Response* res) {
+	server.Get("/", [](Link::Request* req, Link::Response* res) {
 		res->SetHeader("Content-Type", "text/html; charset=UTF-8");
-		res->SendFile("www/index.html");
-        printf("Landing page");
+		std::ifstream file("../www/index.html");
+		std::string content((std::istreambuf_iterator<char>(file)),
+			std::istreambuf_iterator<char>());
+		res->SetBody(content);
 	});
 
-	http.Get("/repos", [](Request* req, Response* res) {
+	
+	server.Get("/repos", [](Link::Request* req, Link::Response* res) {
 		res->SetHeader("Content-Type", "text/html; charset=UTF-8");
-		res->SendFile("www/repolist.html");
-        printf("Repo list page");
+		std::ifstream file("../www/repolist.html");
+		std::string content((std::istreambuf_iterator<char>(file)),
+				std::istreambuf_iterator<char>());
+		res->SetBody(content);
 	});
 
-	http.Get("/mailinglists", [](Request* req, Response* res) {
+	server.Get("/mailinglists", [](Link::Request* req, Link::Response* res) {
 		res->SetHeader("Content-Type", "text/html; charset=UTF-8");
-		res->SendFile("www/mailinglists.html");
+		std::ifstream file("../www/mailinglists.html");
+		std::string content((std::istreambuf_iterator<char>(file)),
+				std::istreambuf_iterator<char>());
+		res->SetBody(content);
 	});
 
-	http.Get("/css/index.css", [](Request* req, Response* res) {
-		res->SetHeader("Content-Type", "text/css; charset=UTF-8");
-		res->SendFile("www/css/index.css");
-	});
-
-	http.Get("/css/fonts.css", [](Request* req, Response* res) {
-		res->SetHeader("Content-Type", "text/css; charset=UTF-8");
-		res->SendFile("www/css/fonts.css");
-	});
-
-	// repository page
-	http.Get("/aristonl/gitserv", [](Request* req, Response* res) {
+	server.Get("/aristonl/gitserv", [](Link::Request* req, Link::Response* res) {
 		res->SetHeader("Content-Type", "text/html; charset=UTF-8");
-		res->SendFile("www/repository.html");
+		std::ifstream file("../www/repository.html");
+		std::string content((std::istreambuf_iterator<char>(file)),
+				std::istreambuf_iterator<char>());
+		res->SetBody(content);
 	});
 
-	// assets
-	http.Get("/assets/img/n11-sm-white.svg", [](Request* req, Response* res) {
-		res->SetHeader("Content-Type", "image/svg+xml");
-		res->SendFile("www/assets/img/n11_sm_white.svg");
-	});
-
-	http.Get("/assets/fonts/JetBrainsMono-Italic-VariableFont_wght.ttf",
-		[](Request* req, Response* res) {
-			res->SetHeader("Content-Type", "font/ttf");
-			res->SendFile(
-				"www/assets/fonts/JetBrainsMono-Italic-VariableFont_wght.ttf");
-		});
-
-	http.Get("/assets/fonts/JetBrainsMono-VariableFont_wght.ttf",
-		[](Request* req, Response* res) {
-			res->SetHeader("Content-Type", "font/ttf");
-			res->SendFile(
-				"www/assets/fonts/JetBrainsMono-VariableFont_wght.ttf");
-		});
+	// TODO: maybe look into moving assets into public/?
+	server.SetStaticPages("../www/");
 
 	std::cout << "gitserv started on port 3000." << std::endl;
-	http.Start();
+	server.Start();
 }
